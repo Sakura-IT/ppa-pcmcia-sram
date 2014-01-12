a798a916e673d7bfc85c005c16856f4a044de37c
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2014-01-12 13:41:35 +0100

    Put CIS tuples for RAM card into array. Simplify code.

--- src/cis.vhd
+++ src/cis.vhd
@@ -1,5 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY cis IS
 	PORT ( d : OUT  STD_LOGIC_VECTOR (7 downto 0);
@@ -10,25 +11,40 @@ ENTITY cis IS
 END cis;
 
 ARCHITECTURE behavioral OF cis IS
-	TYPE mem IS ARRAY (0 to 2**4) OF STD_LOGIC_VECTOR(7 downto 0);
+	TYPE mem IS ARRAY (0 to 31) OF STD_LOGIC_VECTOR(7 downto 0);
 	CONSTANT cis_rom_ram : mem := (
-		0 => "00000001",
-		1 => "00000011",
-		2 => "00000010",
-		3 => "00000101",
-		4 => "00000110",
-		5 => "00000111",
-		6 => "00000000",
-		7 => "00000001",
-		8 => "00000000",
-		9 => "00000001",
-		10 => "00000000",
-		11 => "00000001",
-		12 => "00000000",
-		13 => "00000001",
-		14 => "00000000",
-		15 => "00000001",
-		16 => "00000000");
+		0 => "00000001",	-- tuple: CIS device
+		1 => "00000011",	-- tuple: link to next
+		2 => "01100000",	-- SRAM 100 ns
+		3 => "00001110",	-- 4MB
+		4 => "11111111",	-- tuple: end
+		5 => "00010101",	-- tuple: version 1
+		6 => "00001101",	-- tuple: link to next
+		7 => "00000100",	-- PCMCIA 1.x
+		8 => "00000001",	-- PCMCIA 1.1
+		9 => "01010000",	-- P
+		10 => "01010000",	-- P
+		11 => "01000001",	-- A
+		12 => "00000000",	-- NULL delimeter
+		13 => "01010011",	-- S
+		14 => "00000000",	-- R
+		15 => "01010010",	-- A
+		16 => "01000001",	-- M
+		17 => "00000000",	-- NULL delimeter
+		18 => "00000000",	-- NULL delimeter
+		19 => "11111111",	-- tuple: end
+		20 => "11111111",	-- tuple: end of chain
+		21 => "11111111",
+		22 => "11111111",
+		23 => "11111111",
+		24 => "11111111",
+		25 => "11111111",
+		26 => "11111111",
+		27 => "11111111",
+		28 => "11111111",
+		29 => "11111111",
+		30 => "11111111",
+		31 => "11111111");
 	CONSTANT cis_rom_disk : mem := (
 		0 => "00000000",
 		1 => "00000001",
@@ -46,41 +62,26 @@ ARCHITECTURE behavioral OF cis IS
 		13 => "00000001",
 		14 => "00000000",
 		15 => "00000001",
-		16 => "00000000");
+		16 => "00000000",
+		17 => "00000000",
+		18 => "00000000",
+		19 => "00000000",
+		20 => "00000000",
+		21 => "11111111",
+		22 => "11111111",
+		23 => "11111111",
+		24 => "11111111",
+		25 => "11111111",
+		26 => "11111111",
+		27 => "11111111",
+		28 => "11111111",
+		29 => "11111111",
+		30 => "11111111",
+		31 => "11111111");
 BEGIN
 
-	d <= cis_rom_ram(0) WHEN a="00000" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(1) WHEN a="00001" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(2) WHEN a="00010" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(3) WHEN a="00011" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(4) WHEN a="00100" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(5) WHEN a="00101" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(6) WHEN a="00110" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(7) WHEN a="00111" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(8) WHEN a="01000" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(9) WHEN a="01001" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(10) WHEN a="01010" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(11) WHEN a="01011" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(12) WHEN a="01100" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(13) WHEN a="01101" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(14) WHEN a="01110" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_ram(15) WHEN a="01111" AND oe='0' AND cs='0' AND mode='0' ELSE
-		 cis_rom_disk(0) WHEN a="00000" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(1) WHEN a="00001" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(2) WHEN a="00010" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(3) WHEN a="00011" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(4) WHEN a="00100" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(5) WHEN a="00101" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(6) WHEN a="00110" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(7) WHEN a="00111" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(8) WHEN a="01000" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(9) WHEN a="01001" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(10) WHEN a="01010" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(11) WHEN a="01011" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(12) WHEN a="01100" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(13) WHEN a="01101" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(14) WHEN a="01110" AND oe='0' AND cs='0' AND mode='1' ELSE
-		 cis_rom_disk(15) WHEN a="01111" AND oe='0' AND cs='0' AND mode='1' ELSE
+	d <= cis_rom_ram(TO_INTEGER(UNSIGNED(a))) WHEN oe='0' AND cs='0' AND mode='0' ELSE
+		 cis_rom_disk(TO_INTEGER(UNSIGNED(a))) WHEN oe='0' AND cs='0' AND mode='1' ELSE
 		"ZZZZZZZZ";
 
 END behavioral;
